bb832659f53fdef70a9cb9bfafff0748b7c885b1
JoaquinReyes2905 <mikemoble2905@gmail.com>
2023-09-08 15:00:31 -0300

    add

--- Game/1942/Game1.cs
+++ Game/1942/Game1.cs
@@ -1,16 +1,25 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
 
 namespace _1942
 {
     public class Game1 : Game
     {
+        Texture2D enemigo;
+        int posicionX = 0;
+        int posicionY = 0;
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+        DateTime timew;
+        DateTime tiempoActual;
+        TimeSpan tiempo;
+
 
         public Game1()
         {
+
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
@@ -25,19 +34,35 @@ namespace _1942
 
         protected override void LoadContent()
         {
+
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+            enemigo = Content.Load<Texture2D>("img/player");
+            timew = DateTime.Now;
+
+
+
         }
 
         protected override void Update(GameTime gameTime)
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
+            
+            tiempoActual = DateTime.Now;
+            tiempo = tiempoActual - timew;
+            if (tiempo.Milliseconds >= 300)
+            {
+                timew = DateTime.Now;
+                posicionX += 10;
+                posicionY += 2;
+            }
 
             // TODO: Add your update logic here
 
             base.Update(gameTime);
+            
         }
 
         protected override void Draw(GameTime gameTime)
@@ -45,7 +70,9 @@ namespace _1942
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
-
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(enemigo, new Vector2(posicionX, posicionY), Color.White);
+            _spriteBatch.End();
             base.Draw(gameTime);
         }
     }
